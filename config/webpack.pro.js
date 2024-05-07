136425c23eab5097a8b4e0db1d20d1967f7ae8b4
youngs <lixiang@popicorns.com>
2024-05-07 17:24:25 +0800

    learn:webpack oneOf

--- config/webpack.pro.js
+++ config/webpack.pro.js
@@ -34,77 +34,81 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        //use 数组里面的loader 执行顺序是从右到左，从后面往前面执行
-        use: getStyleLoaders(),
-      },
-      {
-        test: /\.less$/i,
-        use: getStyleLoaders("less-loader"),
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: getStyleLoaders("sass-loader"),
-      },
-      {
-        test: /\.styl$/,
-        use: getStyleLoaders("stylus-loader"),
-      },
-      {
-        test: /\.(png|jpe?g|gif|webp|svg)/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 150 * 1024,
+        oneOf: [
+          {
+            test: /\.css$/,
+            //use 数组里面的loader 执行顺序是从右到左，从后面往前面执行
+            use: getStyleLoaders(),
           },
-        },
-        generator: {
-          // 将图片文件输出到 static/imgs 目录中
-          // 将图片文件命名 [hash:8][ext][query]
-          // [hash:8]: hash值取8位
-          // [ext]: 使用之前的文件扩展名
-          // [query]: 添加之前的query参数
-          filename: "static/imgs/[hash:8][ext][query]",
-        },
-        // parser: {
-        //   dataUrlCondition: {
-        //     maxSize: 150 * 1024, // 4kb
-        //   },
-        // },
-      },
-      {
-        test: /\.(woff|ttf|woff2)/,
-        type: "asset/resource",
-        generator: {
-          // 将图片文件输出到 static/fonts 目录中
-          // 将图片文件命名 [hash:8][ext][query]
-          // [hash:8]: hash值取8位
-          // [ext]: 使用之前的文件扩展名
-          // [query]: 添加之前的query参数
-          filename: "static/fonts/[hash:8][ext][query]",
-        },
-      },
-      {
-        test: /\.(mp3|mp4)/,
-        type: "asset/resource",
-        generator: {
-          // 将图片文件输出到 static/fonts 目录中
-          // 将图片文件命名 [hash:8][ext][query]
-          // [hash:8]: hash值取8位
-          // [ext]: 使用之前的文件扩展名
-          // [query]: 添加之前的query参数
-          filename: "static/medea/[hash:8][ext][query]",
-        },
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          //   options: {
-          //     presets: ["@babel/preset-env"],
-          //   },
-        },
+          {
+            test: /\.less$/i,
+            use: getStyleLoaders("less-loader"),
+          },
+          {
+            test: /\.s[ac]ss$/,
+            use: getStyleLoaders("sass-loader"),
+          },
+          {
+            test: /\.styl$/,
+            use: getStyleLoaders("stylus-loader"),
+          },
+          {
+            test: /\.(png|jpe?g|gif|webp|svg)/,
+            type: "asset",
+            parser: {
+              dataUrlCondition: {
+                maxSize: 150 * 1024,
+              },
+            },
+            generator: {
+              // 将图片文件输出到 static/imgs 目录中
+              // 将图片文件命名 [hash:8][ext][query]
+              // [hash:8]: hash值取8位
+              // [ext]: 使用之前的文件扩展名
+              // [query]: 添加之前的query参数
+              filename: "static/imgs/[hash:8][ext][query]",
+            },
+            // parser: {
+            //   dataUrlCondition: {
+            //     maxSize: 150 * 1024, // 4kb
+            //   },
+            // },
+          },
+          {
+            test: /\.(woff|ttf|woff2)/,
+            type: "asset/resource",
+            generator: {
+              // 将图片文件输出到 static/fonts 目录中
+              // 将图片文件命名 [hash:8][ext][query]
+              // [hash:8]: hash值取8位
+              // [ext]: 使用之前的文件扩展名
+              // [query]: 添加之前的query参数
+              filename: "static/fonts/[hash:8][ext][query]",
+            },
+          },
+          {
+            test: /\.(mp3|mp4)/,
+            type: "asset/resource",
+            generator: {
+              // 将图片文件输出到 static/fonts 目录中
+              // 将图片文件命名 [hash:8][ext][query]
+              // [hash:8]: hash值取8位
+              // [ext]: 使用之前的文件扩展名
+              // [query]: 添加之前的query参数
+              filename: "static/medea/[hash:8][ext][query]",
+            },
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: "babel-loader",
+              //   options: {
+              //     presets: ["@babel/preset-env"],
+              //   },
+            },
+          },
+        ],
       },
     ],
   },
