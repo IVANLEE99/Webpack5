a89f106a1503ee14dadebdb8909d5e44a0593ac8
youngs <lixiang@popicorns.com>
2024-05-07 10:47:05 +0800

    learn:webpack asset/resource

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import "./sass/index.scss";
 import "./sass/index.sass";
 import "./stylus/index.styl";
 import "./iconfont/iconfont.css";
+import "./medea/sgs002.mp4";
+import url from "./medea/xkfy.mp3";
 
 console.log(count(2, 1));
 console.log(sum(1, 2, 3, 4, 5, 9));
+console.log(url);
--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,18 @@ module.exports = {
           filename: "static/fonts/[hash:8][ext][query]",
         },
       },
+      {
+        test: /\.(mp3|mp4)/,
+        type: "asset/resource",
+        generator: {
+          // 将图片文件输出到 static/fonts 目录中
+          // 将图片文件命名 [hash:8][ext][query]
+          // [hash:8]: hash值取8位
+          // [ext]: 使用之前的文件扩展名
+          // [query]: 添加之前的query参数
+          filename: "static/medea/[hash:8][ext][query]",
+        },
+      },
     ],
   },
   //插件
